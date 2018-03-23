6d2e02f4f5084d38a64a65b76fd6107f8561259e
Patrycja <patrycja.kretkiewicz@gmail.com>
2018-03-23 15:58:22 +0100

    Attributes- CategoriesList changes

--- src/components/attribute-sets/CategoriesList.vue
+++ src/components/attribute-sets/CategoriesList.vue
@@ -1,7 +1,16 @@
 <template>
     <div id="side-menu">
         <ul>
-            <li v-for="category in categories">{{category.name}}</li>
+            <li v-for="category in categories" class="cat-item">
+                <input type="checkbox" v-model="selectedMainCategories" :value="category.id">
+                <label :class="{'label': true, 'label inactive': category.visibility == 0} ">{{category.name}}</label>
+                <ul class="children">
+                <li v-for="child in category.children">
+                    <input type="checkbox" v-model="selectedChildren" :value="child.id">
+                    <label :class="{'label': true, 'label inactive': child.visibility == 0}">{{child.name}}</label>
+                </li>
+            </ul>
+            </li>
         </ul>
     </div>
 </template>
@@ -10,7 +19,9 @@
   export default {
     name: 'categories-list',
     data: () => ({
-      categories: []
+      categories: [],
+      selectedMainCategories: [],
+      selectedChildren: []
     }),
     created: function () {
       axios('categories').then(result => {
@@ -26,4 +37,56 @@
         width: 350px;
         margin-left: -5px;
     }
+   .cat-item li{
+       margin-bottom: 15px;
+       margin-left: -10px;
+   }
+   #side-menu li{
+       margin-bottom: 15px;
+   }
+
+   .children{
+       padding-top: 10px;
+   }
+   .inactive{
+       color: #B9B7B9;
+   }
+   /*.cat-item input{*/
+       /*visibility: hidden;*/
+   /*}*/
+
+
+    /*.cust-check{*/
+        /*cursor: pointer;*/
+        /*position: relative;*/
+        /*width: 35px;*/
+        /*height: 35px;*/
+        /*top: 0;*/
+        /*left: 0;*/
+        /*background: #FFFFFF;*/
+        /*border:1px solid #ddd;*/
+        /*padding-right: 17px;*/
+        /*border-radius: 3px;*/
+    /*}*/
+
+     /*.cust-check::after {*/
+        /*opacity: 1;*/
+        /*content: '';*/
+        /*position: absolute;*/
+        /*width: 9px;*/
+        /*height: 5px;*/
+        /*background: transparent;*/
+        /*top: 3px;*/
+        /*left: 3px;*/
+        /*border: 3px solid #333;*/
+        /*border-top: none;*/
+        /*border-right: none;*/
+
+        /*transform: rotate(-45deg);*/
+    /*}*/
+    /*.cat-item input[type=checkbox]:checked + .cust-check:after {*/
+        /*opacity: 1;*/
+    /*}*/
+
+
 </style>
